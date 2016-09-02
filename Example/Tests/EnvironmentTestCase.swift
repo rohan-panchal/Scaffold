9e64003a2ec4fe38e2b949fff194cd2ce6c3e157
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-01 21:12:28 -0500

    Example tests.

---
+++ Example/Tests/EnvironmentTestCase.swift
@@ -0,0 +1,26 @@
+//
+//  EnvironmentTestCase.swift
+//  Scaffold
+//
+//  Created by Panchal, Rohan on 9/1/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Scaffold
+
+class EnvironmentTestCase: XCTestCase {
+    
+    func testEnvironmentManagerInitializeEnvironmentDefault() {
+        EnvironmentManager.initializeEnvironment()
+        
+        XCTAssert(EnvironmentManager.sharedManager.environment == .Development)
+    }
+    
+    func testEnvironmentManagerInitializeEnvironmentNonDefaultValue() {
+        EnvironmentManager.initializeEnvironment(.Production)
+        
+        XCTAssert(EnvironmentManager.sharedManager.environment == .Development)
+    }
+    
+}
