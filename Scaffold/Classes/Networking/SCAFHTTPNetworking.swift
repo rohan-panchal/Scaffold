605d5b09d3fce52283095a2fa304407a112e15ce
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-06 21:29:31 -0500

    Added HTTP Method and Encoding enums.

---
+++ Scaffold/Classes/Networking/SCAFHTTPNetworking.swift
@@ -0,0 +1,23 @@
+//
+//  SCAFHTTPNetworking.swift
+//  Scaffold
+//
+//  Created by Rohan Panchal on 9/6/16.
+//
+//
+
+import Foundation
+
+public enum SCAFHTTPMethod {
+    case GET
+    case POST
+    case PUT
+    case PATCH
+    case DELETE
+}
+
+public enum SCAFHTTPEncoding {
+    case URL
+    case URLEncoded
+    case JSON
+}
