942b0a53898c0c8a29307d08dcfc4525cd10eea0
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-09 14:22:17 -0500

    IBInspectable attributes and UITextField generator method.

--- Scaffold/Classes/UIKit/Extensions/UIKit+Extensions.swift
+++ Scaffold/Classes/UIKit/Extensions/UIKit+Extensions.swift
@@ -10,20 +10,72 @@ import UIKit
 
 extension UIView {
     
-    public class func view(frame: CGRect = CGRectZero) -> UIView {
-        let view = UIView(frame: frame)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    public func addSubviews(views: [UIView]) {
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable public var borderColor: CGColor? {
+        get {
+            return self.layer.borderColor
+        }
+        set {
+            self.layer.borderColor = newValue
+        }
     }
     
 }
 
 extension UIView {
     
-    public func addSubviews(views: [UIView]) {
-        for view in views {
-            self.addSubview(view)
-        }
+    public class func view(frame: CGRect = CGRectZero,
+                           translatesAutoresizingMaskIntoConstraints: Bool = false) -> UIView {
+        
+        let view = UIView(frame: frame)
+        view.translatesAutoresizingMaskIntoConstraints = translatesAutoresizingMaskIntoConstraints
+        
+        return view
+    }
+    
+}
+
+extension UITextField {
+    
+    public class func textField(frame: CGRect = CGRectZero,
+                                translatesAutoresizingMaskIntoConstraints: Bool = false,
+                                text: String = "",
+                                placeholder: String = "",
+                                borderStyle: UITextBorderStyle = .RoundedRect,
+                                textColor: UIColor = UIColor.blackColor()) -> UITextField {
+        
+        let textField = UITextField(frame: frame)
+        textField.translatesAutoresizingMaskIntoConstraints = translatesAutoresizingMaskIntoConstraints
+        
+        textField.text = text
+        textField.placeholder = placeholder
+        textField.borderStyle = borderStyle
+        textField.textColor = textColor
+        
+        return textField
     }
     
 }
