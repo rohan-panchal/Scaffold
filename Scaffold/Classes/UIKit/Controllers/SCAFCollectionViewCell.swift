6961774716783e59ba054d7571b79469c9e6c8b3
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-12 19:57:35 -0500

    Created SCAFCollectionViewCell.

---
+++ Scaffold/Classes/UIKit/Controllers/SCAFCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  SCAFCollectionViewCell.swift
+//  Pods
+//
+//  Created by Rohan Panchal on 9/12/16.
+//
+//
+
+import UIKit
+
+public class SCAFCollectionViewCell: UICollectionViewCell {
+    
+    public class func defaultReuseIdentifer() -> String {
+        return self.className
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+}
+
+extension SCAFCollectionViewCell: UIScaffold {
+    
+    private func setup() {
+        self.contentView.addSubviews(self.initialSubviews())
+        self.setupConstraints(self.contentView)
+        self.setupActions()
+        self.setupColorScheme()
+        self.setLocalizedCopy()
+    }
+    
+    public func initialSubviews() -> [UIView] {
+        return []
+    }
+    
+    public func setupConstraints(rootView: UIView) {
+    }
+    
+    public func setupActions() {
+    }
+    
+    public func setupColorScheme() {
+    }
+    
+    public func setLocalizedCopy() {
+    }
+    
+}
+
