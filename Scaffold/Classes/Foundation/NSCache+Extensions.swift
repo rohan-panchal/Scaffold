2ee2c10e462552ce84f225d5d3b41ff06f86b8d9
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-15 13:12:56 -0500

    Added NSCache+Extensions.

---
+++ Scaffold/Classes/Foundation/NSCache+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  NSCache+Extensions.swift
+//  Pods
+//
+//  Created by Panchal, Rohan on 9/15/16.
+//
+//
+
+import Foundation
+
+extension NSCache {
+    
+    subscript(key: AnyObject) -> AnyObject? {
+        get {
+            return objectForKey(key)
+        }
+        set {
+            if let value: AnyObject = newValue {
+                setObject(value, forKey: key)
+            } else {
+                removeObjectForKey(key)
+            }
+        }
+    }
+    
+}
