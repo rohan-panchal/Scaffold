c38d5b746db0f9f05f1a9644a2eaf05a43cd257e
Rohan Panchal <rohan@rohanpanchal.com>
2016-09-20 20:32:01 -0500

    UIAlertController+Extensions added.

---
+++ Scaffold/Classes/UIKit/Extensions/UIAlertController+Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  UIAlertController+Extensions.swift
+//  Scaffold
+//
+//  Created by Rohan Panchal on 9/20/16.
+//
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    public func actionSheetController(title: String?,
+                                      message: String?,
+                                      actions: [UIAlertAction]? = []) -> UIAlertController {
+        return alertControllerGenerator(title,
+                                        message: message,
+                                        preferredStyle: .ActionSheet,
+                                        actions: actions)
+    }
+    
+    public func alertController(title: String?,
+                                message: String?,
+                                actions: [UIAlertAction]? = []) -> UIAlertController {
+        return alertControllerGenerator(title,
+                                        message: message,
+                                        preferredStyle: .Alert,
+                                        actions: actions)
+    }
+    
+    public func alertControllerGenerator(title: String?,
+                                message: String?,
+                                preferredStyle: UIAlertControllerStyle = .Alert,
+                                actions: [UIAlertAction]? = []) -> UIAlertController {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        
+        if let actions = actions {
+            for action in actions {
+                controller.addAction(action)
+            }
+        }
+        
+        return controller
+    }
+    
+}
