7ceb81dbf92b2d80e8fa0997ecedab1fc23beddf
Rohan Panchal <rohan@rohanpanchal.com>
2016-12-28 13:08:31 -0600

    Added documentation to NSObject+Extensions.

--- Scaffold/Classes/Foundation/NSObject+Extensions.swift
+++ Scaffold/Classes/Foundation/NSObject+Extensions.swift
@@ -10,15 +10,19 @@ import Foundation
 
 extension NSObject {
     
-    /*
-     * The name of the class.
+    /**
+     The name of the class.
+     
+     @return The String name of the class.
      */
     public class var className: String {
         return NSStringFromClass(self).components(separatedBy: ".").last!
     }
     
-    /*
-     * The name of the class.
+    /**
+     The name of the class.
+     
+     @return The String name of the class.
      */
     public var className: String {
         return NSStringFromClass(type(of: self)).components(separatedBy: ".").last!
